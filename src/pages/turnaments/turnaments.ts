bc6391f0d2951db770a02e6ca579b16483f01180
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-08-07 18:56:46 +0300

    add loader animation ,,,,when data is geting form firebase

--- src/pages/turnaments/turnaments.ts
+++ src/pages/turnaments/turnaments.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MyTeamsPage } from "../my-teams/my-teams";
 import { TeamsPage } from "../teams/teams";
 import { EliteApi } from "../../providers/elite-api/elite-api";
@@ -16,13 +16,23 @@ public turnaments:any;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              private eliteApi:EliteApi) {
+              private eliteApi:EliteApi,
+              public loadingController:LoadingController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TurnamentsPage');
-this.eliteApi.getTurnaments().then(data=>this.turnaments=data);
+     let loader=this.loadingController.create({
+       content:"Getting turnaments ..",
+       ///spinner:"dots"
+     });
 
+     loader.present().then(()=>{
+       this.eliteApi.getTurnaments().then(
+       data=>{
+         this.turnaments=data;
+         loader.dismiss();
+       });
+     });
   }
 
 
