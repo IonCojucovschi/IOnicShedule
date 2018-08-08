e038842651e5c4a9b09c18ad265e779433af0901
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-08-08 12:53:56 +0300

    add teams list ,,, add types of button ,,,, add  custom list view

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TeamHomePage } from "../team-home/team-home";
 import { EliteApi } from "../../providers/elite-api/elite-api";
-
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-teams',
@@ -11,21 +11,41 @@ import { EliteApi } from "../../providers/elite-api/elite-api";
 export class TeamsPage {
 
 public teams=[];
-
-
+private allTeams:any;
+private allTeamDivision:any;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
+              private loadingController:LoadingController,
               private eliteApi:EliteApi) {
   }
 
   ionViewDidLoad() {
 
     let selectedTurney=this.navParams.data;
-    this.eliteApi.getTuramentData(selectedTurney.id).subscribe(data=>{
-      this.teams=data.teams;
 
-    })
+    let loader=this.loadingController.create({
+      content:'Geting data..'
+    });
+
+    loader.present().then(()=>{
+    this.eliteApi.getTuramentData(selectedTurney.id).subscribe(data=>{
+      this.allTeams=data.teams;
+
+      this.allTeamDivision=_.chain(data.teams)
+                          .groupBy('division')
+                          .toPairs()
+                          .map(item=>_.zipObject(['divisionName','divisionTeams'],item))
+                          .value();
+
+      this.teams=this.allTeamDivision;
+      console.log('Division TEams', this.teams);
+      loader.dismiss();
+
+     });
+    });
+
+    
     console.log('ionViewDidLoad TeamsPage');
   }
 
