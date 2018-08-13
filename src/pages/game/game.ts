5b7632f2395ed857053d6d2a14a531a7b89c2f35
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-08-13 17:08:23 +0300

    game page view ()

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -19,7 +19,7 @@ public game :any={};
 
   ionViewDidLoad() {
 this.game=this.navParams.data;
-
+this.game.gameTime=Date.parse(this.game.time);
   }
   teamTapped(teamId)
   {
@@ -27,4 +27,25 @@ this.game=this.navParams.data;
     let team=turneyData.teams.find(t=>t.id===teamId);
     this.navCtrl.push(TeamHomePage,team);
   }
+
+goToDirections()
+{
+  //placeholder
+}
+ 
+goToMap()
+{
+  //placeholder
+} 
+
+isWinner(score1,score2)
+{
+  return Number(score1)>Number(score2) ? 'primary' : 'danger';
+}
+
+
+
+
+
+
 }
